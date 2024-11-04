09c851e31a6d87e54773f1f4f0e3aa05dac29044
Ethan Muller <ethanbmuller@gmail.com>
2024-11-04 18:54:19 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  export let data: PageData;
+</script>
+
+{#each data.posts as post}
+  <p><audio controls src="{post.file.url}">{post.notes}</audio></p>
+{/each}
---
+++ src/routes/+page.ts
@@ -0,0 +1,18 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch }) => {
+  const response = await fetch('http://localhost:3000/api/graphql', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: "query { posts { notes file {filename url} } }",
+    }),
+  })
+  const result = await response.json()
+
+	return {
+    posts: result.data.posts
+	};
+};
