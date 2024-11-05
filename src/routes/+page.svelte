c7d90471611d0aac9ae6d2ed96dfd60da9089a6f
Ethan Muller <ethanbmuller@gmail.com>
2024-11-05 02:18:52 -0500

    use tags (only renders when public + audio)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
   import type { PageData } from './$types';
   export let data: PageData;
+
+  function isTagApplied(tags, tag) {
+    return tags.includes(tag);
+  }
+
+  function isPublic(tags) {
+    return isTagApplied(tags, 'public')
+  }
+
+  function isAudio(tags) {
+    return isTagApplied(tags, 'audio')
+  }
+
+  function isLoop(tags) {
+    return isTagApplied(tags, 'loop')
+  }
+
 </script>
 
 {#each data.posts as post}
-  <div class="c-post"><audio controls src="{post.file.url}"></audio>{post.notes}</div>
+  {#if isPublic(post.tags) && isAudio(post.tags)}
+    <div class="c-post"><audio controls src="{post.file.url}"></audio>{post.notes}</div>
+  {/if}
 {/each}
